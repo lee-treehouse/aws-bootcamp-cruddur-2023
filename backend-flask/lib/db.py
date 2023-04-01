80c14095852a515527c400ed7a26b2698075b18a
lee-treehouse <leentaylor@gmail.com>
2023-04-01 20:24:05 +1100

    tidy and return to pointing at localhost where user with handle andrewbrown exists

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -26,8 +26,7 @@ class Db:
   def init_pool(self):
     connection_url = os.getenv("CONNECTION_URL")
     self.pool = ConnectionPool(connection_url)
-  # we want to commit data such as an insert
-  # be sure to check for RETURNING in all uppercases
+
   def print_params(self,params):
     blue = '\033[94m'
     no_color = '\033[0m'
@@ -40,6 +39,7 @@ class Db:
     no_color = '\033[0m'
     print(f'{cyan} SQL STATEMENT-[{title}]------{no_color}')
     print(sql)
+  
   def query_commit(self,sql,params={}):
     self.print_sql('commit with returning',sql)
 
@@ -57,6 +57,7 @@ class Db:
           return returning_id
     except Exception as err:
       self.print_sql_err(err)
+  
   # when we want to return a json object
   def query_array_json(self,sql,params={}):
     self.print_sql('array',sql)
@@ -67,6 +68,7 @@ class Db:
         cur.execute(wrapped_sql,params)
         json = cur.fetchone()
         return json[0]
+            
   # When we want to return an array of json objects
   def query_object_json(self,sql,params={}):
 
@@ -82,6 +84,7 @@ class Db:
           "{}"
         else:
           return json[0]
+        
   def query_wrap_object(self,template):
     sql = f"""
     (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
@@ -89,6 +92,7 @@ class Db:
     ) object_row);
     """
     return sql
+  
   def query_wrap_array(self,template):
     sql = f"""
     (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
@@ -96,6 +100,7 @@ class Db:
     ) array_row);
     """
     return sql
+  
   def print_sql_err(self,err):
     # get details about the exception
     err_type, err_obj, traceback = sys.exc_info()
