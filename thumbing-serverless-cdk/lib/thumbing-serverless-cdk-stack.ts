c7b95fceb62382d979eae4e4703fbbb0fe9409c4
lee-treehouse <leentaylor@gmail.com>
2023-04-22 20:43:19 +1000

    create a s3 bucket - can see the cfn if you run cdk synth

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -1,16 +1,21 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import {Construct} from "constructs";
 
 export class ThumbingServerlessCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const bucketName: string = process.env.THUMBING_BUCKET_NAME as string;
+    const bucket = this.createBucket(bucketName);
+  }
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ThumbingServerlessCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+  createBucket(bucketName: string) {
+    const bucket = new s3.Bucket(this, "ThumbingBucket", {
+      bucketName,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+    return bucket;
   }
 }
