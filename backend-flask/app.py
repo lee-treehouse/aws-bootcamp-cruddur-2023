2dce785788f7002272b2a47c3ecca93d282f8d23
lee-treehouse <leentaylor@gmail.com>
2023-04-24 08:27:40 +1000

    comment out init rollbar til i can come back to it

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -93,6 +93,7 @@ RequestsInstrumentor().instrument()
 
 frontend = os.getenv("FRONTEND_URL")
 backend = os.getenv("BACKEND_URL")
+# need to add www prefix for frontend url as well
 origins = [frontend, backend]
 cors = CORS(
     app,
@@ -113,22 +114,22 @@ cors = CORS(
 rollbar_access_token = os.getenv("ROLLBAR_ACCESS_TOKEN")
 
 
-@app.before_first_request
-def init_rollbar():
-    """init rollbar module"""
-    rollbar.init(
-        # access token
-        rollbar_access_token,
-        # environment name
-        "production",
-        # server root directory, makes tracebacks prettier
-        root=os.path.dirname(os.path.realpath(__file__)),
-        # flask already sets up logging
-        allow_logging_basic_config=False,
-    )
+# @app.before_first_request
+# def init_rollbar():
+#     """init rollbar module"""
+#     rollbar.init(
+#         # access token
+#         rollbar_access_token,
+#         # environment name
+#         "production",
+#         # server root directory, makes tracebacks prettier
+#         root=os.path.dirname(os.path.realpath(__file__)),
+#         # flask already sets up logging
+#         allow_logging_basic_config=False,
+#     )
 
-    # send exceptions from `app` to rollbar, using flask's signal system.
-    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+#     # send exceptions from `app` to rollbar, using flask's signal system.
+#     got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 
 @app.route("/api/health-check")
