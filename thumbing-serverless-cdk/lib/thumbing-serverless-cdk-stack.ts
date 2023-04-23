37b0fd1d398fa2e33bd39050378c42083617775a
lee-treehouse <leentaylor@gmail.com>
2023-04-24 09:55:56 +1000

    add dotenv

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -1,14 +1,22 @@
 import * as cdk from "aws-cdk-lib";
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 import {Construct} from "constructs";
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 
 export class ThumbingServerlessCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-    const bucketName: string = process.env.THUMBING_BUCKET_NAME as string;
+    const bucketName = process.env.THUMBING_BUCKET_NAME!;
+    const functionPath = process.env.THUMBING_FUNCTION_PATH!;
+    const folderInput = process.env.THUMBING_FOLDER_INPUT!;
+    const folderOutput = process.env.THUMBING_FOLDER_OUTPUT!;
+
     const bucket = this.createBucket(bucketName);
+    const lambdaFunc = this.createLambda(bucketName, functionPath, folderInput, folderOutput);
   }
 
   createBucket(bucketName: string) {
@@ -17,5 +25,26 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
     return bucket;
+  }
+
+  createLambda(
+    bucketName: string,
+    functionPath: string,
+    folderInput: string,
+    folderOutput: string
+  ) {
+    const myLambda = new lambda.Function(this, "ThumbingLambda", {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: "index.handler",
+      code: lambda.Code.fromAsset(functionPath),
+      environment: {
+        DEST_BUCKET_NAME: bucketName,
+        FOLDER_INPUT: folderInput,
+        FOLDER_OUTPUT: folderOutput,
+        PROCESS_WIDTH: "512",
+        PROCESS_HEIGHT: "512",
+      },
+    });
+    return myLambda;
   }
 }
